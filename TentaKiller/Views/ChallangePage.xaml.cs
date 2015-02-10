f225fb933e896caca83043d0b993f75e798d69ac
Hannes Landstedt a.k.a. zarac <git@zarac.se>
2015-02-10 14:06:35 +0100

    [fix] Challange files

---
+++ TentaKiller/Views/ChallangePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TentaKiller.Models;
+
+namespace TentaKiller.Views
+{
+    /// <summary>
+    /// Interaction logic for ChallangePage.xaml
+    /// </summary>
+    public partial class ChallangePage : Page
+    {
+        protected MainWindow mainWindow;
+
+        protected Challange challange;
+        public Challange Challange { get { return challange; } set { challange = value; DataContext = value; } }
+
+        public ChallangePage(MainWindow window)
+        {
+            mainWindow = window;
+            InitializeComponent();
+        }
+    }
+}
